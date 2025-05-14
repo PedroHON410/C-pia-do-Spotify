fca0f102610053145ed4cd7ea34b27f33e546af7
PedroHON410 <pedro.hon200511@gmail.com>
2025-05-14 14:03:27 -0300

    Primeiro commit spotify

---
+++ src/App2.jsx
@@ -0,0 +1,27 @@
+// Arrow function
+// const App2 = () => <h1>Olá mundo!</h1>;
+// rafce
+// Nomeação de componentes
+// PascalCase
+// Export defalt posso importar com qualquer nome e sem chaves
+// Export sem defalt só posso importar entre chaves e com nome exportado
+// Self clousing tag
+// <Header></Header>;
+// <Header />
+
+/*import React from "react";
+
+function App2() {
+  return <div>App2</div>;
+}
+
+export default App2;
+*/
+
+//Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Modifiers
+// Bloco_elemento -- modificador
+// header__link
+// header__link -- small
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import LogoSpotify from "../assets/Pasta Assets com Database e Logo/logo/spotify-logo.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <a href="">
+        <img src={LogoSpotify} alt="Logo do Spotify" />
+      </a>
+
+      <a href="">
+        <h1>Spotify</h1>
+      </a>
+    </div>
+  );
+};
+
+export default Header;
